a13bbb78bd42e6d8b30258301f1893e9e25bd53a
Viktor <67584100+Viktor85-prog@users.noreply.github.com>
2021-04-20 21:02:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let acc=document.getElementsByClassName('accordion');
+for (let i=0; i<acc.length;i++){
+    acc[i].onclick = function() {
+        this.classList.toggle('active')
+        this.nextElementSibling.classList.toggle('show')
+    }
+}
